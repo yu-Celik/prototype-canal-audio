7aaec34277cfbb49040856d3a4be3141d6ad3028
yu-Celik <celik.yus@outlook.com>
2024-12-23 22:36:37 +0100

    #

--- api/websocket.js
+++ api/websocket.js
@@ -1,27 +1,35 @@
-import WebSocket from 'ws';
+import { WebSocketServer } from 'ws';
 import { AudioServer } from '../server.js';
 
 const audioServer = new AudioServer();
 
 export default function handler(req, res) {
-    console.log('WebSocket handler called');
-    if (res.socket.server.ws) {
-        // Le WebSocket est déjà configuré
-        res.end();
-        console.log('WebSocket already configured');
-        return;
+    if (!res.socket.server.ws) {
+        // Configuration du WebSocket Server
+        const wss = new WebSocketServer({
+            noServer: true
+        });
+
+        // Gestion de l'upgrade HTTP vers WebSocket
+        res.socket.server.on('upgrade', (request, socket, head) => {
+            wss.handleUpgrade(request, socket, head, (ws) => {
+                wss.emit('connection', ws, request);
+            });
+        });
+
+        // Gestion des connexions
+        wss.on('connection', (ws) => {
+            audioServer.handleConnection(ws);
+        });
+
+        res.socket.server.ws = wss;
     }
 
-
-    const wss = new WebSocket.Server({ server: res.socket.server });
-    res.socket.server.ws = wss;
-
-    console.log('WebSocket configured');
-    wss.on('connection', (ws) => {
-        console.log('New connection');
-        audioServer.handleConnection(ws);
-    });
-
-    console.log('WebSocket handler finished');
     res.end();
-} 
+}
+
+export const config = {
+    api: {
+        bodyParser: false
+    }
+};
