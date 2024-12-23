0d53d921a5cc498f3e9c7a9f94888cd1e3f7fe19
yu-Celik <celik.yus@outlook.com>
2024-12-23 22:45:24 +0100

    #

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
-    WEBSOCKET_URL: 'wss://prototype-canal-audio.vercel.app/api/websocket.js'
+    WEBSOCKET_URL: 'wss://prototype-canal-audio.vercel.app/api/websocket'
 };
 
 export default config; 
